80c80b95c77c980cb27810c570406ebde801f645
wiseosagie <wiseosagie@yahoo.com>
2018-03-02 19:36:16 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,6 +33,6 @@ passport.deserializeUser(Account.deserializeUser());
 app.use('/v1', routes);
 
 app.server.listen(config.port);
-//console.log(`Started on port ${app.server.address().port}`);
+console.log(`Started on port ${app.server.address().port}`);
 
 export default app;
